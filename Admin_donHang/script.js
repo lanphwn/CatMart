b54b41cf20a33e720ae17b882d6fbf08b96b323b
phuongnl <nguyenlanphuong19082003@gmail.com>
2023-12-28 09:49:00 +0700

    add

--- Admin_donHang/script.js
+++ Admin_donHang/script.js
@@ -16,13 +16,11 @@ function openTable(evt, tableName) {
     evt.currentTarget.className += " active";
 }
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
